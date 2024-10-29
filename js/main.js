ea1c200c557bf0704c9dbd6ba03bb13e61bf2d47
SoryeongYoo <soryung11@naver.com>
2024-10-29 13:47:27 +0900

    navigations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import ipads from '../data/ipads.js'
-
+import navigations from '../data/navigations.js'
 /*장바구니*/
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
@@ -145,4 +145,29 @@ ipads.forEach(function (ipad) {
     
 
   itemsEl.append(itemEl) //만들어진 itemEl를 itemsEl 배열에 넣어줌
+})
+
+
+const navigationsEl = document.querySelector('footer .navigations')
+navigations.forEach(function (nav) {
+  const mapEl=document.createElement('div')
+  mapEl.classList.add('map')
+
+  let mapList = ''
+  nav.maps.forEach(function(map) {
+    mapList +=/* html */ `<li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`
+  })
+
+  mapEl.innerHTML = /* html */`
+    <h3>
+      <span class="text">${nav.title}</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `
+  
+  navigationsEl.append(mapEl)
 })
